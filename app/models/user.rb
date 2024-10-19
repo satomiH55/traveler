599d1dfb8349d8cf6fc88b7f3f8f2fc736d2f20d
satomiH55 <180079359+satomiH55@users.noreply.github.com>
2024-10-19 17:21:12 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  as_many :following_user, through: :follower, source: :followed
+  has_many :following_user, through: :follower, source: :followed
   has_many :follower_user, through: :followed, source: :follower
 
   attachment :profile_image
@@ -24,5 +24,5 @@ class User < ApplicationRecord
 
   def following?(user)
     following_user.include?(user)
-
+  end
 end
